7d9761d533149575b4838cf82eeed565f7ce0e63
Fred Vadon <fvadon@nuxeo.com>
2015-04-13 18:50:07 -0400

    Feature: added very simple widget and seam bean

---
+++ nuxeo-labs-external-reference-core/src/main/java/org/nuxeo/labs/reference/web/ExternalReferencesBean.java
@@ -0,0 +1,29 @@
+package org.nuxeo.labs.reference.web;
+
+import java.io.Serializable;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.labs.reference.operation.AbstractExternalReferenceOperation;
+
+
+/**
+ * This Seam bean manages the external references.
+ *
+ * @author <a href="mailto:fvadon@nuxeo.com">Fred Vadon</a>
+ */
+@Name("externalReferences")
+@Scope(ScopeType.CONVERSATION)
+public class ExternalReferencesBean extends AbstractExternalReferenceOperation implements Serializable {
+
+    public DocumentModelList getExternalReferences(String DocumentId){
+        return getExternalReferenceInfo(DocumentId, null);
+    }
+
+
+
+    private static final long serialVersionUID = 1L;
+
+}
